8b2c30ca227634799494b59edcc05875d7e62ee7
Siarhei Lunski <loonskai@gmail.com>
2019-12-18 20:41:13 +0300

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,56 @@
+import { commands, ExtensionContext, StatusBarItem, StatusBarAlignment, window } from 'vscode';
+
+class StatusbarUi {
+
+	private static _statusBarItem: StatusBarItem;
+
+	static get statusbar() {
+		if (!StatusbarUi._statusBarItem) {
+			StatusbarUi._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left, 100);
+			this.statusbar.show();
+		}
+
+		return StatusbarUi._statusBarItem;
+	}
+
+	static Init() {
+		setTimeout(function () {
+				StatusbarUi.Stopped();
+		}, 1000);
+	}
+
+	public static Recording() {
+		StatusbarUi.statusbar.text = '$(circle-filled) Recording';
+		StatusbarUi.statusbar.command = 'extension.stopRecording';
+		StatusbarUi.statusbar.tooltip = 'Stop comment recording';
+}
+
+	public static Stopped() {
+			StatusbarUi.statusbar.text = '$(play) Record a comment';
+			StatusbarUi.statusbar.command = 'extension.startRecording';
+			StatusbarUi.statusbar.tooltip = 'Start recording a comment';
+	}
+}
+
+export function activate(context: ExtensionContext) {
+
+	const statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+	statusBarItem.command = 'extension.startRecording';
+	StatusbarUi.Stopped();
+
+	const startRecording = commands.registerCommand('extension.startRecording', () => {
+		StatusbarUi.Recording();
+		window.showInformationMessage('Start recording');
+	});
+
+	const stopRecording = commands.registerCommand('extension.stopRecording', () => {
+		StatusbarUi.Stopped();
+		window.showInformationMessage('Stop recording');
+	});
+
+	context.subscriptions.push(startRecording);
+	context.subscriptions.push(stopRecording);
+
+}
+
+export function deactivate() {}

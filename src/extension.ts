522686f39d70ee018fd8b3b0e0c4050aa398473d
Siarhei Lunski <loonskai@gmail.com>
2019-12-19 14:02:12 +0300

    Insert output message

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import {
   commands,
   ExtensionContext,
   window,
+  Position
 } from "vscode";
 import Recognizer from './recognizer';
 import StatusBar from './statusbar';
@@ -9,7 +10,17 @@ import StatusBar from './statusbar';
 export function activate(context: ExtensionContext): void {
   const token = process.env.REV_AI_TOKEN || '';
   const recognizer = new Recognizer(token, StatusBar, (message: string) => {
-    window.showInformationMessage(message);
+    const { activeTextEditor } = window;
+    if (activeTextEditor) {
+      const { line, character } = activeTextEditor.selection.active;
+      
+      activeTextEditor.edit(() => {
+        const startPosition = new Position(line, character + 1);
+        activeTextEditor.edit(editor => {
+          editor.insert(startPosition, `// ${message}`)
+        });
+      });
+    }
   });
 
   const startRecording = commands.registerCommand(

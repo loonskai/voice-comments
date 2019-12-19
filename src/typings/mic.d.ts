92d2a4b5a5a87853ade9883eb33afce5732893b1
Siarhei Lunski <loonskai@gmail.com>
2019-12-19 06:06:53 +0300

    Add mic.d.ts

--- src/typings/mic.d.ts
+++ src/typings/mic.d.ts
@@ -1,4 +1,22 @@
 declare module 'mic' {
-  const x: any;
+  interface MicOptions {
+    endian?: 'big' | 'little';
+    bitwidth?: string;
+    encoding?: 'signed-integer' | 'unsigned-integer';
+    rate?: string;
+    channels?: string;
+    device?: string;
+    exitOnSilence?: number;
+    debug?: boolean;
+    fileType?: string;
+  }
+
+  interface MicInstance {
+    getAudioStream: () => NodeJS.ReadWriteStream;
+    start: () => void;
+    stop: () => void;
+  }
+
+  const x: (options: MicOptions) => MicInstance;
   export = x;
 }
